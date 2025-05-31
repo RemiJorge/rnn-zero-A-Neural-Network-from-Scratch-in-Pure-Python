18e026ff71125853d73871ec0b43e3cabd286740
remi.jorge <remi.jorge@etu.umontpellier.fr>
2025-05-31 19:04:21 +0200

    feat: validation and example of use

--- src/neural_network.py
+++ src/neural_network.py
@@ -11,6 +11,7 @@ from src.optimizers.rmsprop import Optimizer_RMSprop
 from src.validations.softmax_crossentropy import SoftmaxCrossentropy
 from src.validations.accuracy import Accuracy
 
+import numpy as np
 
 class NeuralNetwork:
 
@@ -51,15 +52,29 @@ class NeuralNetwork:
         self.optimizer.post_update_params()
 
 
-    def train(self, inputs, y_true):
+    def train(self, inputs, y_true, validation=0.1):
 
-        self.forward(inputs)
-        loss = self.softmax_loss.forward(self.outputs, y_true)
-        acc = self.accuracy.calculate(self.softmax_loss.outputs, y_true)
+        indices = np.arange(len(inputs))
+        np.random.shuffle(indices)
+        inputs = inputs[indices]
+        y_true = y_true[indices]
+        split_index = int(len(inputs) * (1 - validation))
+        x_train, x_val = inputs[:split_index], inputs[split_index:]
+        y_train, y_val = y_true[:split_index], y_true[split_index:]
+
+        self.forward(x_train)
+        loss = self.softmax_loss.forward(self.outputs, y_train)
+        acc = self.accuracy.calculate(self.softmax_loss.outputs, y_train)
         
-        self.dinputs = self.softmax_loss.backward(self.softmax_loss.outputs, y_true)
+        self.dinputs = self.softmax_loss.backward(self.softmax_loss.outputs, y_train)
         self.backward(self.dinputs)
         self.update_optimizer()
+
+        if validation > 0:
+            self.forward(x_val)
+            val_loss = self.softmax_loss.forward(self.outputs, y_val)
+            val_acc = self.accuracy.calculate(self.softmax_loss.outputs, y_val)
+            return loss, acc, val_loss, val_acc
 
         return loss, acc
 
